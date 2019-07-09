76ecadb973a21336fe6ffac9b9712154e8776294
poushitaguha <pguha.kolkata@gmail.com>
2019-07-09 18:33:53 +0000

    Add code for exercises 5 and 6

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,11 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+total_revenue = Store.sum(:annual_revenue)
+puts "Total Revenue: $#{total_revenue}"
+
+avg_revenue = Store.average(:annual_revenue)
+puts "Average Annual Revenue: $#{avg_revenue}"
+
+num_stores_over_million = Store.where("annual_revenue > 1000000").count
+puts "No. of stores generating more than $1M: #{num_stores_over_million}"
--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,14 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@store4 = Store.find_by(id: 4)
+
+@store1.employees.create(first_name: "Srimanta", last_name: "Bagchi", hourly_rate: 82)
+@store1.employees.create(first_name: "Poushita", last_name: "Guha", hourly_rate: 60)
+@store1.employees.create(first_name: "Sajal", last_name: "Guha", hourly_rate: 92)
+@store2.employees.create(first_name: "Rita", last_name: "Guha", hourly_rate: 56)
+@store2.employees.create(first_name: "Sudeb", last_name: "Bagchi", hourly_rate: 88)
+@store2.employees.create(first_name: "Sarbani", last_name: "Bagchi", hourly_rate: 71)
+@store4.employees.create(first_name: "Srirup", last_name: "Bagchi", hourly_rate: 54)
+@store4.employees.create(first_name: "Utpal", last_name: "Guha", hourly_rate: 55)
+@store4.employees.create(first_name: "Manisha", last_name: "Guha", hourly_rate: 22)
