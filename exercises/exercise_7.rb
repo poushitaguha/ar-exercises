4ef9b1807f238f2e0cf133f3d85a46fb7250b39c
poushitaguha <pguha.kolkata@gmail.com>
2019-07-09 21:01:25 +0000

    Add code for exercise 7

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,15 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+puts "Enter a store name:"
+store_name = gets.chomp
+
+@newStore = Store.create(name: store_name, annual_revenue: 3000000, mens_apparel: false, womens_apparel: true)
+@newStore.errors.full_messages.each do |error|
+  puts error
+end
+
+# @newEmployee = Employee.create(first_name: "Jack")
+# @newEmployee.errors.full_messages.each do |error|
+#   puts error
+# end
--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,15 @@
 class Store < ActiveRecord::Base
     has_many :employees
+
+    # Validations
+    validate :mens_or_womens_apparel_present
+    validates :name, length: { minimum: 3 }
+    validates :annual_revenue, numericality: { only_integer: true, greater_than_or_equal_to: 0 } 
+
+    def mens_or_womens_apparel_present
+        if mens_apparel == false && womens_apparel == false
+          errors.add(:mens_apparel, "should be present in the store OR")
+          errors.add(:womens_apparel, "should be present in the store")
+        end
+    end    
 end
